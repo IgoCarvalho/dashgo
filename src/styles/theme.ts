22872919025849bd9d1db44337a9240a06219005
Igo Carvalho <igocarvalho00@gmail.com>
2023-03-17 03:58:22 -0300

    feat: extend chakra theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,27 @@
 import { extendTheme } from '@chakra-ui/react';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] });
 
 export const theme = extendTheme({
+  colors: {
+    gray: {
+      900: '#181B23',
+      800: '#1F2029',
+      700: '#353646',
+      600: '#4B4D63',
+      500: '#616480',
+      400: '#797D9A',
+      300: '#9699B0',
+      200: '#B3B5C6',
+      100: '#D1D2DC',
+      50: '#EEEEF2',
+    },
+  },
+  fonts: {
+    heading: roboto.style.fontFamily,
+    body: roboto.style.fontFamily,
+  },
   styles: {
     global: {
       body: {
