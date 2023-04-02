56796ffe208635873c4d53cb680387aa49756988
Igo Carvalho <igocarvalho00@gmail.com>
2023-04-01 22:58:35 -0300

    feat: add miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,14 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import { makeServer } from '@/services/mirage';
 import { SidebarDrawerProvider } from '@/contexts/SidebarDrawerContext';
 
 import { theme } from '@/styles/theme';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
