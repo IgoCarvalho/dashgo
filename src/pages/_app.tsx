176ac91415ceb0b607e7fa8e1f79548c8b376af6
Igo Carvalho <igocarvalho00@gmail.com>
2023-04-14 19:28:56 -0300

    feat: add React Query Devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { makeServer } from '@/services/mirage';
 import { SidebarDrawerProvider } from '@/contexts/SidebarDrawerContext';
@@ -30,6 +31,8 @@ export default function App({ Component, pageProps }: AppProps) {
             <Component {...pageProps} />
           </SidebarDrawerProvider>
         </ChakraProvider>
+
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </>
   );
